076cd0709c53047465a16ab48b16aac82ead264f
Jonmott2u4u <jmott@umass.edu>
2024-02-23 13:47:32 -0500

    Added quartz hit position plots for each ring

--- PlotExtractor.C
+++ PlotExtractor.C
@@ -6,13 +6,10 @@ void PlotExtractor()
     std::ifstream rfiles("files.dat");
     std::string line;
     TFile *file;
-
     TH1D *hst, *tmp;
 
     while(std::getline(rfiles, line)){
-
         file = TFile::Open(line.data());
-
         for(int det=1; det<9; det++){
 
             //Unedited PE spectrums
@@ -21,24 +18,27 @@ void PlotExtractor()
             hst->SetTitle(Form("R%i Photoelectron Distribution",det));
             hst->GetXaxis()->SetTitle("Photoelectrons");
             hst->GetXaxis()->SetTitle("Events");
-            hst->GetXaxis()->SetRangeUser(1,100);
+            hst->GetXaxis()->SetRangeUser(0,100);
             hst->SetDirectory(0);
-            hst->SaveAs(Form("plots/R%i_raw_spectrum.root",det));
+            //hst->SaveAs(Form("plots/R%i_raw_spectrum.root",det));
 
             //PE spectrum for beams that hit only one quartz tile
             tmp = (TH1D*)file->Get(Form("R%iOnly_CathodeEventsDistrHist",det));
             hst = (TH1D*)tmp->Clone(Form("R%i_Solo_PEs",det));
-            hst->SetTitle("R%i Photoelectron Distribution");
+            hst->SetTitle(Form("R%i Photoelectron Distribution",det));
             hst->GetXaxis()->SetTitle("Photoelectrons");
             hst->GetXaxis()->SetTitle("Events");
-            hst->GetXaxis()->SetRangeUser(1,100);
+            hst->GetXaxis()->SetRangeUser(0,100);
             hst->SetDirectory(0);
-            hst->SaveAs(Form("plots/R%i_quartz_cut.root",det));
+            //hst->SaveAs(Form("plots/R%i_quartz_cut.root",det));
 
+            TTree *tree = (TTree*)file->Get("MOLLEROptTree");
+            cout << tree << endl;
+            TCanvas *canvas = new TCanvas("canvas","canvas");
+            tree->Draw(Form("MOLLEROptData.MOLLERDetectorEvent.R%iTileHitY:MOLLEROptData.MOLLERDetectorEvent.R%iTileHitX",det,det),"","colz");
+            canvas->SaveAs(Form("R%i_quartz_hit_pos.root",det));
         }
-
+        
         file->Close("R");
-
     }
-
 }
